3c8bf5c95d3406e213c2aed9172c460a82946600
Ivan <iresonancei@mail.com>
2020-04-25 01:37:18 +0300

    push

--- index.js
+++ index.js
@@ -6,45 +6,35 @@ easyvk({
     token: 'bda38eb5ec9d73e6d70f6a89d0d4f8641231eae48cf249ab2a6288693b86ca2b92bd767918c8dd203804e'
 }).then(async vk => {
 
-    console.log(vk)
-
     const connection = await vk.callbackAPI.listen({
         confirmCode: '3b575cfd',
         port: process.env.PORT || 8080,
-        path: '/webhook',
+        path: '/',
         app
     })
-    //
-    // connection.on('new_message', msg => {
-    //     console.log(msg)
-    // })
 
 })
 
-// easyvk({
-//     token: 'bda38eb5ec9d73e6d70f6a89d0d4f8641231eae48cf249ab2a6288693b86ca2b92bd767918c8dd203804e',
-//     sessionFile: path.join(__dirname, '.session'),
-//     utils: {callbackAPI: true}
-// }).then(async vk => {
-//     // const data = await vk.call('messages.send', {
-//     //     peer_id: '199595040',
-//     //     message: 'Hello, how are you?',
-//     //     random_id: easyvk.randomId()
-//     // })
+
+
+//     .then(async vk => {
+//
+//     console.log(vk)
+//
+//     const connection = await vk.callbackAPI.listen({
+//         confirmCode: '3b575cfd',
+//         port: process.env.PORT || 8080,
+//         path: '/webhook',
+//         app
+//     })
 //     //
-//     // console.log(data)
-// })
+//     // connection.on('new_message', msg => {
+//     //     console.log(msg)
+//     // })
 //
-//
-// easyvk.callbackAPI.listen({
-//     // port: process.ENV.port || 8080,
-//     groupId: 74248643,
-//     confirmCode: '3b575cfd',
-//     app
-// }).then(connection => {
-//     // connection.on('new_message', console.log)
 // })
 
-app.get('/', (req, res) => {
-    res.send('Hello2')
-})
+
+// app.get('/', (req, res) => {
+//     res.send('Hello2')
+// })
