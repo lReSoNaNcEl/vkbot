c4215d6ae69bb6ed9665f0248d445ad49858f688
Ivan <iresonancei@mail.com>
2020-04-25 04:03:54 +0300

    push

--- index.js
+++ index.js
@@ -4,21 +4,16 @@ const app = require('express')()
 
 easyvk({
     token: 'bda38eb5ec9d73e6d70f6a89d0d4f8641231eae48cf249ab2a6288693b86ca2b92bd767918c8dd203804e',
-    v: 5.103,
-    confirmCode: '3b575cfd'
 }).then(async vk => {
 
+        console.log(vk)
 
-    // let connection = await vk.bots.longpoll.connect();
-
-    const connection = await vk.bots.longpoll.connect();
-
-    // const connection = await vk.callbackAPI.listen({
-    //     confirmCode: '3b575cfd',
-    //     port: process.env.PORT || 8080,
-    //     path: '/',
-    //     app
-    // })
+    const connection = await vk.callbackAPI.listen({
+        confirmCode: '3b575cfd',
+        port: process.env.PORT || 8080,
+        path: '/',
+        app
+    })
 
     connection.on('message_new', async msg => {
 
