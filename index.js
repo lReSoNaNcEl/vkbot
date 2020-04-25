1019cffa19577d0709ec2c67bd27a99ceffae2d2
Ivan <iresonancei@mail.com>
2020-04-25 04:22:33 +0300

    push

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ easyvk({
     token: 'bda38eb5ec9d73e6d70f6a89d0d4f8641231eae48cf249ab2a6288693b86ca2b92bd767918c8dd203804e',
 }).then(async vk => {
 
-        console.log(vk)
-
     const connection = await vk.callbackAPI.listen({
         confirmCode: '3b575cfd',
         port: process.env.PORT || 8080,
@@ -16,14 +14,10 @@ easyvk({
     })
 
     connection.on('message_new', async msg => {
-
-        console.log(msg)
-
         const data = await vk.call('messages.send', {
             peer_id: msg.object.user_id,
             message: 'Привет, привет)',
             random_id: easyvk.randomId()
         })
-
     })
 })
